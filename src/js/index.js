d98bb14c51908a249190a9f4cb5495bae631a158
Baudry Souvignet <76646601+baudrysouvignet@users.noreply.github.com>
2022-11-25 13:07:31 +0100

    slider OK scandale -OK, header OK

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,31 @@
-let carroussel = document.getElementById('contentCarr')
-let timer = Date.now()
-let switchback = document.getElementById('back')
-let switchnext = document.getElementById('next')
-carroussel.style.marginLeft = "0%"
+let carroussel = document.getElementById("contentCarr");
+let timer = Date.now();
+let switchback = document.getElementById("back");
+let switchnext = document.getElementById("next");
+carroussel.style.marginLeft = "0%";
 
 function switchvisibility() {
-    if (carroussel.style.marginLeft == "calc(0%)") {
-        switchback.style.visibility = "collapse";
-    } else if (carroussel.style.marginLeft == "calc(-400%)") {
-        switchnext.style.visibility = "collapse";
-    } else {
-        switchback.style.visibility = "initial";
-        switchnext.style.visibility = "initial";
-    }
+  if (carroussel.style.marginLeft == "calc(0%)") {
+    switchback.style.visibility = "collapse";
+  } else if (carroussel.style.marginLeft == "calc(-400%)") {
+    switchnext.style.visibility = "collapse";
+  } else {
+    switchback.style.visibility = "initial";
+    switchnext.style.visibility = "initial";
+  }
 }
 
 function switchcar(choix) {
-    if (timer - Date.now() < -600) {
-        if (choix == "next") {
-            carroussel.style.marginLeft = "Calc(" + carroussel.style.marginLeft + " - 100%";
-            switchvisibility();
-        } else {
-            carroussel.style.marginLeft = "Calc(" + carroussel.style.marginLeft + " + 100%";
-            switchvisibility();
-        }
-        timer = Date.now();
+  if (timer - Date.now() < -600) {
+    if (choix == "next") {
+      carroussel.style.marginLeft =
+        "Calc(" + carroussel.style.marginLeft + " - 100%";
+      switchvisibility();
+    } else {
+      carroussel.style.marginLeft =
+        "Calc(" + carroussel.style.marginLeft + " + 100%";
+      switchvisibility();
     }
+    timer = Date.now();
+  }
 }
