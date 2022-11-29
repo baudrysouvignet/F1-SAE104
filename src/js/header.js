77a7d72e1d6c2c0ec0d79a017636a692c0b8c24b
Baudry Souvignet <76646601+baudrysouvignet@users.noreply.github.com>
2022-11-29 20:08:22 +0100

    anim ok

--- src/js/header.js
+++ src/js/header.js
@@ -11,3 +11,25 @@ function burgerclick() {
         document.getElementById('navheader').className = "close";
     }
 }
+
+
+const ratio = 0.1
+const options = {
+    root: null, /* zonne affichage (l'écran) */
+    rootMargin: '0px',
+    threshold: ratio /* pourcentage de l'élement visible */
+  }
+
+const intersect = function(entries, observer){
+    entries.forEach(function (entry) { /* permet de connaitre le pourcentage de visibilté de l'élément */
+        if (entry.intersectionRatio > ratio){
+            entry.target.classList.add('anim_visible')
+            observer.unobserve(entry.target) /* arreter d'observer une fois visible */
+        }
+    });
+}
+
+let observer = new IntersectionObserver(intersect, options);
+document.querySelectorAll('[class*="anim"]').forEach(function(reveal){
+    observer.observe(reveal)/*  boucle tout les élement avce la class .anim */
+}) 
